f8c86187bbebda21c170e8f3f78d7ed5757ffbc3
minu <min.uswachoke@gmail.com>
2023-08-09 10:05:17 +0200

    refs #1: duplicate project;

---
+++ java/build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    kotlin("android") version "1.8.20" apply false
+    kotlin("android.extensions") version "1.8.20" apply false
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
+}
