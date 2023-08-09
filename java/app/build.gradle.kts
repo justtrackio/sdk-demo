cf03d5f3ccfa5a8706eff7bc3fe669b6080f7244
minu <min.uswachoke@gmail.com>
2023-08-09 11:49:38 +0200

    refs #1: remove coroutine;

--- java/app/build.gradle.kts
+++ java/app/build.gradle.kts
@@ -42,9 +42,6 @@ dependencies {
     //justtrack-sdk
     implementation("io.justtrack:justtrack-android-sdk:4.4.0")
 
-    //coroutine-sdk
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
