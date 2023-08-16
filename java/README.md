803ce4712ad223f74a0d3ff83a85425734bc1ffe
Min Uswachoke <min@justtrack.io>
2023-08-16 08:24:20 +0000

    refs #1: Add java demo;

---
+++ java/README.md
@@ -0,0 +1,30 @@
+# justtrack SDK for Android
+
+This is the [justtrack](https://justtrack.io/) SDK for Android.
+==========
+
+Documentation can be found [here](https://docs.justtrack.io/sdk/android-sdk-readme).
+
+This is a Java demo application that will help you integrate with the justtrack SDK. It provides an
+example of how to utilize the different functions of the SDK.
+
+Install
+--------
+Check out our latest SDK version [here](https://docs.justtrack.io/sdk/android-sdk-readme/android-sdk-changelog).
+
+Install via Gradle:
+```groovy
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://sdk.justtrack.io/maven")
+        }
+    }
+}
+```
+And 
+```groovy
+dependencies {
+    implementation("io.justtrack:justtrack-android-sdk:4.4.0")
+}
+```
---
+++ java/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        namespace = "io.justtrack.demo"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+
+    //justtrack-sdk
+    implementation("io.justtrack:justtrack-android-sdk:4.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
